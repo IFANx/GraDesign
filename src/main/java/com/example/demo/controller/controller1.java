780ccc7975fb2bbb7826aa0108be9eda9480102b
IFANx <47766440+IFANx@users.noreply.github.com>
2021-04-17 17:00:36 +0800

    Update controller1.java

--- src/main/java/com/example/demo/controller/controller1.java
+++ src/main/java/com/example/demo/controller/controller1.java
@@ -2,7 +2,7 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//merge 测试
 @RestController
 public class controller1 {
     @GetMapping("/test1")
